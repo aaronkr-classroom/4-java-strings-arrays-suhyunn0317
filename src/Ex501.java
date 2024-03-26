11c59cd05e243dcd279ac8b1defd20605687182e
suhyunn0317 <chojs3927@naver.com>
2024-03-26 17:07:19 +0900

    finish good

---
+++ src/Ex501.java
@@ -0,0 +1,18 @@
+
+public class Ex501 {
+	// My method
+	public static void hello(String name) {
+		System.out.println("Hello, " + name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hello("Aaron");
+		hello("Tom");
+		hello("Loki");
+		hello("Rocky");
+		hello("Socky");
+		hello(" ");
+	}
+
+}
